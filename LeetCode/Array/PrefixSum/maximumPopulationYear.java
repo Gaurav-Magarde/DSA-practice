10fee927e00162f355d47a97c3c628cb9758fac8
Gaurav-Magarde <gauravmagarde709@gmail.com>
2025-09-27 21:43:08 +0530

    make the solution better

--- LeetCode/Array/PrefixSum/maximumPopulationYear.java
+++ LeetCode/Array/PrefixSum/maximumPopulationYear.java
@@ -1,6 +1,14 @@
 // Leetcode 1854. Maximum Population Year
 
-//
+//You are given a 2D integer array logs where each logs[i] = [birthi, deathi] indicates the birth and death years of the ith person.
+
+//The population of some year x is the number of people alive during that year. The ith person is counted in year x's population if x is in the inclusive range [birthi, deathi - 1]. Note that the person is not counted in the year that they die.
+
+//Return the earliest year with the maximum population.
+
+//  TC = O(n+101)
+
+//  SC = O(1)
 package LeetCode.Array.PrefixSum;
 class maximumPopulationYear{
     public static void main(String[] args){
